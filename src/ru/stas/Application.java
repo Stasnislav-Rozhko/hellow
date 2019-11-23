980be7e79c25ad076f1377d1f5011a7e0e7087cb
Andrey <scorpion812@yandex.ru>
2019-11-23 16:55:56 +0300

    321

--- src/ru/stas/Application.java
+++ src/ru/stas/Application.java
@@ -2,6 +2,7 @@ package ru.stas;
 
 public class Application {
     public static void main (String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Hello worlds");
     }
 }
